4adcdfe2feb3da5ad8489340633c157d6921bafd
Pavlo Hamov <p.hamov@venstar.com>
2021-05-06 16:28:39 +0300

    getValue locked

--- src/main.c
+++ src/main.c
@@ -3,9 +3,14 @@
 #include <pthread.h>
 #include <unistd.h>
 
+static pthread_mutex_t s_mux = PTHREAD_MUTEX_INITIALIZER;
+
 static int getValue(void) {
+    pthread_mutex_lock(&s_mux);
     static int v = 0;
-    return v++;
+    int rv = v++;
+    pthread_mutex_unlock(&s_mux);
+    return rv;
 }
 
 static void *routine(void *arg) {
@@ -17,7 +22,7 @@ static void *routine(void *arg) {
 }
 
 int main(int argc, char **argv) {
-    for (int i = 0; i < 1; ++i) {
+    for (int i = 0; i < 5; ++i) {
         pthread_t tid;
         pthread_create(&tid, NULL, routine, (void*)10000);
     }
